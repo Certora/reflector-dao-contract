54c0a3df302e72087e98a068100299b2c22e4def
Gadi Auerbach <gadiauerbach@Gadis-MacBook-Pro.local>
2024-10-06 20:26:20 +0300

    bug report

--- src/certora/spec.rs
+++ src/certora/spec.rs
@@ -4,15 +4,16 @@ use crate::{DAOContract, BallotStatus, get_ballot};
 use crate::types::{
     ballot_category::BallotCategory, ballot_init_params::BallotInitParams, contract_config::ContractConfig
 };
-use cvt::{require, assert, satisfy};
+//use cvt::{require, assert, satisfy};
+use cvt::{require, assert};
 use cvt_soroban::{is_auth};
 
 
-#[no_mangle]
-pub fn certora_config_sanity(env: Env, config: ContractConfig) {
-    DAOContract::config(env, config);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_config_sanity(env: Env, config: ContractConfig) {
+//     DAOContract::config(env, config);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_config_can_only_be_called_once(env: Env, config: ContractConfig) {
@@ -23,12 +24,12 @@ pub fn certora_config_can_only_be_called_once(env: Env, config: ContractConfig) 
     assert!(false);
 }
 
-#[no_mangle]
-pub fn certora_create_ballot_sanity(env: Env, params: BallotInitParams) {
-    require!(is_auth(params.initiator.clone()), "authorized");
-    DAOContract::create_ballot(env, params);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_create_ballot_sanity(env: Env, params: BallotInitParams) {
+//     require!(is_auth(params.initiator.clone()), "authorized");
+//     DAOContract::create_ballot(env, params);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_create_ballot_must_be_initiator(env: Env, params: BallotInitParams) {    
@@ -49,13 +50,13 @@ pub fn certora_ballot_id_increasing(env: Env, params: BallotInitParams) {
     assert!(after == before + 1);
 }
 
-#[no_mangle]
-pub fn certora_retract_ballot_sanity(env: Env, ballot_id: u64) {
-    let ballot = DAOContract::get_ballot(env.clone(), ballot_id);
-    require!(is_auth(ballot.initiator), "authorized");
-    DAOContract::retract_ballot(env, ballot_id);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_retract_ballot_sanity(env: Env, ballot_id: u64) {
+//     let ballot = DAOContract::get_ballot(env.clone(), ballot_id);
+//     require!(is_auth(ballot.initiator), "authorized");
+//     DAOContract::retract_ballot(env, ballot_id);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_retract_ballot_must_be_initiator(env: Env, ballot_id: u64) {
@@ -74,13 +75,13 @@ pub fn certora_retract_ballot_can_only_be_called_once(e: Env, ballot_id: u64) {
     assert!(after == BallotStatus::Retracted);
 }
 
-#[no_mangle]
-pub fn certora_vote_sanity(e: Env, ballot_id: u64, accepted: bool) {
-    require!(is_auth(e.get_admin().unwrap()), "authorized");
-    require!(get_ballot(&e, ballot_id).status != BallotStatus::Retracted, "not retracted");
-    DAOContract::vote(e, ballot_id, accepted);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_vote_sanity(e: Env, ballot_id: u64, accepted: bool) {
+//     require!(is_auth(e.get_admin().unwrap()), "authorized");
+//     require!(get_ballot(&e, ballot_id).status != BallotStatus::Retracted, "not retracted");
+//     DAOContract::vote(e, ballot_id, accepted);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_vote_must_be_admin(env: Env, ballot_id: u64, accepted: bool) {
@@ -96,12 +97,12 @@ pub fn certora_cannot_vote_on_retracted_ballot(e: Env, ballot_id: u64, accepted:
     assert!(false);
 }
 
-#[no_mangle]
-pub fn certora_set_deposit_sanity(env: Env, deposit_params: Map<BallotCategory, i128>) {
-    require!(is_auth(env.get_admin().unwrap()), "authorized");
-    DAOContract::set_deposit(env, deposit_params);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_set_deposit_sanity(env: Env, deposit_params: Map<BallotCategory, i128>) {
+//     require!(is_auth(env.get_admin().unwrap()), "authorized");
+//     DAOContract::set_deposit(env, deposit_params);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_set_deposit_must_be_admin(env: Env, deposit_params: Map<BallotCategory, i128>) {
@@ -110,12 +111,12 @@ pub fn certora_set_deposit_must_be_admin(env: Env, deposit_params: Map<BallotCat
     assert!(false);
 }
 
-#[no_mangle]
-pub fn certora_unlock_sanity(env: Env, developer: Address, operators: Vec<Address>) {
-    require!(is_auth(env.get_admin().unwrap()), "authorized");
-    DAOContract::unlock(env, developer, operators);
-    satisfy!(true);
-}
+// #[no_mangle]
+// pub fn certora_unlock_sanity(env: Env, developer: Address, operators: Vec<Address>) {
+//     require!(is_auth(env.get_admin().unwrap()), "authorized");
+//     DAOContract::unlock(env, developer, operators);
+//     satisfy!(true);
+// }
 
 #[no_mangle]
 pub fn certora_unlock_must_be_admin(env: Env, developer: Address, operators: Vec<Address>) {
