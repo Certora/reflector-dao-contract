c0b9ea76ab1dfe4dceaec088c7ce916e6c70d799
Eric Eilebrecht <eric@certora.com>
2024-10-15 15:15:04 -0700

    Cleanup structs

--- src/certora/spec.rs
+++ src/certora/spec.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{Env, Map, Address, Vec};
+use soroban_sdk::{Env, Map, Address, Vec, String};
 use crate::extensions::env_extensions::EnvExtensions;
 use crate::{DAOContract, BallotStatus, get_ballot};
 use crate::types::{
@@ -8,61 +8,67 @@ use cvt::{require, assert, satisfy};
 use cvt_soroban::{is_auth};
 use cvt_soroban_macros::*;
 
-
 #[rule]
-pub fn certora_config_sanity(env: Env, config: ContractConfig) {
-    DAOContract::config(env, config);
+pub fn certora_config_sanity(env: Env, admin: Address, token: Address, amount: i128, deposit_params: Map<BallotCategory, i128>) {
+    DAOContract::config(env, ContractConfig { admin, token, amount, deposit_params });
     satisfy!(true);
 }
 
 #[rule]
-pub fn certora_config_can_only_be_called_once(env: Env, config: ContractConfig) {
-    DAOContract::config(env.clone(), config.clone());
+pub fn certora_config_can_only_be_called_once(
+    env: Env, 
+    admin1: Address, token1: Address, amount1: i128, deposit_params1: Map<BallotCategory, i128>,
+    admin2: Address, token2: Address, amount2: i128, deposit_params2: Map<BallotCategory, i128>
+) {
+    DAOContract::config(env.clone(), ContractConfig { admin: admin1, token: token1, amount: amount1, deposit_params: deposit_params1 });
     // Second call should fail
-    DAOContract::config(env.clone(), config.clone());
+    DAOContract::config(env.clone(), ContractConfig { admin: admin2, token: token2, amount: amount2, deposit_params: deposit_params2 });
     // Check that the second call failed (i.e., we should not reach this point).
     assert!(false);
 }
 
 #[rule]
-pub fn certora_create_ballot_sanity(env: Env, params: BallotInitParams) {
+pub fn certora_create_ballot_sanity(e: Env, initiator: Address, category: BallotCategory, title: String, description: String) {
+    let params = BallotInitParams { initiator, category, title, description };
     require!(is_auth(params.initiator.clone()), "authorized");
-    DAOContract::create_ballot(env, params);
+    DAOContract::create_ballot(e, params);
     satisfy!(true);
 }
 
 #[rule]
-pub fn certora_create_ballot_must_be_initiator(env: Env, params: BallotInitParams) {    
+pub fn certora_create_ballot_must_be_initiator(e: Env, initiator: Address, category: BallotCategory, title: String, description: String) {    
+    let params = BallotInitParams { initiator, category, title, description };
     require!(!is_auth(params.initiator.clone()), "not authorized");
-    DAOContract::create_ballot(env, params);
+    DAOContract::create_ballot(e, params);
     // create_ballot should have failed because the initiator did not authorize
     assert!(false)
 }
 
 #[rule]
-pub fn certora_ballot_id_increasing(env: Env, params: BallotInitParams) {    
-    let before = env.get_last_ballot_id();
+pub fn certora_ballot_id_increasing(e: Env, initiator: Address, category: BallotCategory, title: String, description: String) {    
+    let params = BallotInitParams { initiator, category, title, description };
+    let before = e.get_last_ballot_id();
     require!(before < u64::MAX, "ballot_id can't overflow");
-    let id = DAOContract::create_ballot(env.clone(), params.clone());
-    let after = env.get_last_ballot_id();
+    let id = DAOContract::create_ballot(e.clone(), params.clone());
+    let after = e.get_last_ballot_id();
     assert!(after == id);
     // Check that the ballot_id is increasing, and that it's increasing *slowly*, so it can't overflow the 64-bit int.
     assert!(after == before + 1);
 }
 
 #[rule]
-pub fn certora_retract_ballot_sanity(env: Env, ballot_id: u64) {
-    let ballot = DAOContract::get_ballot(env.clone(), ballot_id);
+pub fn certora_retract_ballot_sanity(e: Env, ballot_id: u64) {
+    let ballot = DAOContract::get_ballot(e.clone(), ballot_id);
     require!(is_auth(ballot.initiator), "authorized");
-    DAOContract::retract_ballot(env, ballot_id);
+    DAOContract::retract_ballot(e, ballot_id);
     satisfy!(true);
 }
 
 #[rule]
-pub fn certora_retract_ballot_must_be_initiator(env: Env, ballot_id: u64) {
-    let ballot = DAOContract::get_ballot(env.clone(), ballot_id);
+pub fn certora_retract_ballot_must_be_initiator(e: Env, ballot_id: u64) {
+    let ballot = DAOContract::get_ballot(e.clone(), ballot_id);
     require!(!is_auth(ballot.initiator), "not authorized");
-    DAOContract::retract_ballot(env, ballot_id);
+    DAOContract::retract_ballot(e, ballot_id);
     assert!(false);
 }
 
@@ -84,40 +90,46 @@ pub fn certora_vote_sanity(e: Env, ballot_id: u64, accepted: bool) {
 }
 
 #[rule]
-pub fn certora_vote_must_be_admin(env: Env, ballot_id: u64, accepted: bool) {
-    require!(!is_auth(env.get_admin().unwrap()), "not authorized");
-    DAOContract::vote(env, ballot_id, accepted);
+pub fn certora_vote_must_be_admin(e: Env, ballot_id: u64, accepted: bool) {
+    require!(!is_auth(e.get_admin().unwrap()), "not authorized");
+    DAOContract::vote(e, ballot_id, accepted);
     assert!(false);
 }
 
 #[rule]
-pub fn certora_set_deposit_sanity(env: Env, deposit_params: Map<BallotCategory, i128>) {
-    require!(is_auth(env.get_admin().unwrap()), "authorized");
-    DAOContract::set_deposit(env, deposit_params);
+pub fn certora_set_deposit_sanity(e: Env, deposit_params: Map<BallotCategory, i128>) {
+    require!(is_auth(e.get_admin().unwrap()), "authorized");
+    DAOContract::set_deposit(e, deposit_params);
     satisfy!(true);
 }
 
 #[rule]
-pub fn certora_set_deposit_must_be_admin(env: Env, deposit_params: Map<BallotCategory, i128>) {
-    require!(!is_auth(env.get_admin().unwrap()), "not authorized");
-    DAOContract::set_deposit(env, deposit_params);
+pub fn certora_set_deposit_must_be_admin(e: Env, deposit_params: Map<BallotCategory, i128>) {
+    require!(!is_auth(e.get_admin().unwrap()), "not authorized");
+    DAOContract::set_deposit(e, deposit_params);
     assert!(false);
 }
 
 #[rule]
-pub fn certora_unlock_sanity(env: Env, developer: Address, operators: Vec<Address>) {
-    require!(is_auth(env.get_admin().unwrap()), "authorized");
-    DAOContract::unlock(env, developer, operators);
+pub fn certora_set_deposit_must_be_non_negative(e: Env, deposit_params: Map<BallotCategory, i128>, category: BallotCategory) {
+    require!(!is_auth(e.get_admin().unwrap()), "not authorized");
+    DAOContract::set_deposit(e.clone(), deposit_params);
+    assert!(e.get_deposit(category) >= 0);
+}
+
+#[rule]
+pub fn certora_unlock_sanity(e: Env, developer: Address, operators: Vec<Address>) {
+    require!(is_auth(e.get_admin().unwrap()), "authorized");
+    DAOContract::unlock(e, developer, operators);
     satisfy!(true);
 }
 
 #[rule]
-pub fn certora_unlock_must_be_admin(env: Env, developer: Address, operators: Vec<Address>) {
-    require!(!is_auth(env.get_admin().unwrap()), "not authorized");
-    DAOContract::unlock(env, developer, operators);
+pub fn certora_unlock_must_be_admin(e: Env, developer: Address, operators: Vec<Address>) {
+    require!(!is_auth(e.get_admin().unwrap()), "not authorized");
+    DAOContract::unlock(e, developer, operators);
     assert!(false);
 }
-
 
 #[rule]
 pub fn certora_retracted_ballot_cannot_be_retracted(e: Env, ballot_id: u64) {
@@ -161,12 +173,12 @@ pub fn certora_retracted_ballot_was_draft_or_rejected(e: Env, ballot_id: u64) {
     assert!(before.status == BallotStatus::Draft || before.status == BallotStatus::Rejected);
 }
 
-fn invariant_balances_not_negative<F>(env: &Env, claimant: &Address, f: F) where F: FnOnce() {
-    require!(env.get_available_balance(claimant) >= 0, "available balance not negative");
-    require!(env.get_dao_balance() >= 0, "dao balance not negative");
+fn invariant_balances_not_negative<F>(e: &Env, claimant: &Address, f: F) where F: FnOnce() {
+    require!(e.get_available_balance(claimant) >= 0, "available balance not negative");
+    require!(e.get_dao_balance() >= 0, "dao balance not negative");
     f();
-    assert!(env.get_available_balance(claimant) >= 0);
-    assert!(env.get_dao_balance() >= 0);
+    assert!(e.get_available_balance(claimant) >= 0);
+    assert!(e.get_dao_balance() >= 0);
 }
 
 #[rule]
@@ -205,7 +217,13 @@ pub fn certora_invariant_balances_not_negative_claim(e: Env, claimant: Address, 
 }
 
 #[rule]
-pub fn certora_invariant_balances_not_negative_create_ballot(e: Env, claimant: Address, params: BallotInitParams) {
+pub fn certora_invariant_balances_not_negative_create_ballot(e: Env, claimant: Address, initiator: Address, category: BallotCategory) {
+    let params = BallotInitParams {
+        initiator,
+        category,
+        title: String::from_str(&e, "title"),
+        description: String::from_str(&e, "description"),
+    };
     invariant_balances_not_negative(&e, &claimant, || {
         DAOContract::create_ballot(e.clone(), params);
     });
